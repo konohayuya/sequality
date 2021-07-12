b24a17f291621f281616a0f44770793d0eb0013d
konoha <konohayuya1222@gmail.com>
2021-07-12 14:39:34 +0900

    add some test case
    テストケースの追加

--- app/src/test/java/sequality/CaclulateTest.java
+++ app/src/test/java/sequality/CaclulateTest.java
@@ -12,4 +12,29 @@ public class CaclulateTest {
     assertEquals(expected, calclulate.sum(2, 3));
   }
 
+  @Test
+  public void testAverage() {
+    Calclulate calclulate = new Calclulate();
+    assertEquals(5, calclulate.average(10, 2));
+  }
+
+  @Test
+  public void testSumRange() {
+    Calclulate calclulate = new Calclulate();
+    assertEquals(15, calclulate.sumRange(1, 5));
+  }
+
+  @Test
+  public void testSumRangeOdd() {
+    Calclulate calclulate = new Calclulate();
+    assertEquals(9, calclulate.sumRangeOdd(0, 5));
+    assertEquals(9, calclulate.sumRangeOdd(1, 5));
+  }
+
+  @Test
+  public void testSumRangeEven() {
+    Calclulate calclulate = new Calclulate();
+    assertEquals(12, calclulate.sumRangeEven(1, 6));
+    assertEquals(12, calclulate.sumRangeEven(2, 6));
+  }
 }
